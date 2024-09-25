1f9c6b94eedbe30bb5cd77c11c075cb57adbf240
Saurav Wagh <saurav.wagh@tansam.org>
2024-09-25 13:22:51 +0530

    program master done

---
+++ resources/views/admin/masters/sections/create.blade.php
@@ -0,0 +1,81 @@
+@extends('admin.layouts.layout')
+@section('title', 'Create Facility Type')
+@section('content')
+<div class="page-wrapper">
+   <div class="container-fluid">
+    <div class="row page-titles">
+        <div class="col-md-5 align-self-center">
+            <h4 class="text-themecolor">Create Facility Type</h4>
+        </div>
+        <div class="col-md-7 align-self-center text-right">
+            <div class="d-flex justify-content-end align-items-center">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{url('/dashboard')}}">Dashboard</a></li>
+                    <li class="breadcrumb-item active"><a href="{{route('facilitytypes.index')}}">Facility Type</a></li>
+                    <li class="breadcrumb-item active">Create Facility Type</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+      <div class="row">
+        <div class="col-md-12">
+            <div class="card card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <div class="row">
+                <div class="col-sm-12 col-xs-12">
+                    <form action="{{route('facilitytypes.store')}}" enctype="multipart/form-data" method="post">
+                        {{csrf_field()}}
+
+                    <div class="row pt-3">
+                        <div class="form-group col-sm-4 col-xs-4">
+                            <label for="name" class="required">Name</label>
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Enter Name" value="{{old('name')}}">
+                        </div>
+
+                        <div class="form-group col-sm-4 col-xs-4">
+                            <label for="phc_id" class="required">PHC</label>
+                            <select name="phc_id" id="section" class="form-control">
+                                <option value="" >-- Select PHC -- </option>
+                                @foreach($phc as $key => $value)
+                                <option value="{{$value->id}}" {{SELECT($value->id,old('phc_id'))}}>{{$value->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                       
+                        <div class="form-group col-sm-4 col-xs-4">
+                            <label for="facilitytype" class="">Select Image</label>
+                            <input type="file" name="facilitytype_image" class="form-control" id="facilitytype_image accept="image/png,image/jpg,image/jpeg">
+                            <small class="form-control-feedback text-danger"> Accepted only .png/.jpg/.jpeg format & allowed max size is 2MB </small>
+                        </div>
+
+                        <div class="form-group col-sm-4 col-xs-4">
+                            <label for="status" class="required">Status </label>
+                            <select name="status" id="status" class="form-control">
+                                @foreach($statuses as $key => $value)
+                                <option value="{{$value}}" {{SELECT($value,old('status'))}}>{{$key}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                        <hr>
+                        <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
+                       <a type="reset" class="btn btn-inverse waves-effect waves-light" href="{{route('facilitytypes.index')}}"> Cancel </a>
+                    </form>
+                </div>
+            </div>
+        </div>
+          </div>
+        </div>
+   </div>
+</div>
+@endsection
